29819b7513be1dae6dc41a4043a937d47e3f4634
Isaac22 <luisisaacmv22@gmail.com>
2025-04-30 10:30:23 -0600

    se sube parte 2 del ejercicio

---
+++ html/JS/main.js
@@ -0,0 +1,13 @@
+`btn btn-primary my-2`.addEventListener("click", function(event){
+    event.preventDefault();
+    alertResultado.innerHtml =  <br/>
+
+});
+
+fetch(URLMain+catch, options)
+.then ((response)=>{
+    console.log(Response);
+    response.jason().then((res) =>{
+        
+    })
+})
