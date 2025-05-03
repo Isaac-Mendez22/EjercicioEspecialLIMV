2043852755faeb4aeb91073a9ec0df22b839438c
Isaac22 <luisisaacmv22@gmail.com>
2025-05-02 19:14:18 -0600

    se completa la parte 3 del ejercicio

--- html/JS/main.js
+++ html/JS/main.js
@@ -1,3 +1,6 @@
+
+const URLMain = `https://api.escuelajs.co/api/v1/products`
+
 `btn btn-primary my-2`.addEventListener("click", function(event){
     event.preventDefault();
     alertResultado.innerHtml =  <br/>
